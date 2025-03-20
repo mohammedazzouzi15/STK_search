d9b822f7a4dc334153d497777748620e904ba206
mohammedzzouzi15 <mohammed.azzouzi15@ic.ac.uk>
2025-03-20 11:13:33 +0100

    Update .gitignore and add figure modification script for enhanced visualization

---
+++ Example_notebooks/figures/modify_figure.py
@@ -0,0 +1,83 @@
+# Load an existing figure from a file
+# Replace 'example_figure.png' with the path to your saved figure
+# Note: Matplotlib does not directly load figures from image files.
+# If you have the figure saved as a `.pickle` file, you can load it.
+import pickle
+
+import numpy as np
+from matplotlib.ticker import MaxNLocator
+
+with open("fig.pkl", "rb") as f:
+    fig = pickle.load(f)
+
+fig.axes[1].set_xlabel("")
+fig.axes[3].set_xlabel("")
+# Hide tick labels for specific axes
+fig.axes[1].tick_params(labelleft=False)  # Hide bottom and left tick labels
+fig.axes[3].tick_params(labelleft=False)  # Hide bottom and left tick labels
+
+fig.axes[1].set_frame_on(False)
+fig.axes[3].set_frame_on(False)
+min_num_iteration = 1000
+x_limits = {
+    0: (50, min_num_iteration),
+    2: (50, min_num_iteration),
+    4: (50, min_num_iteration),
+    5: (50, min_num_iteration),
+    1: (0, 100),
+    3: (0, 3100),
+}
+y_limits = {
+    0: (0, 0.7),
+    1: (0, 0.7),
+    2: (-2, 1),
+    3: (-2, 1),
+    4: (0, 65),
+    5: (0, 0.63),
+}
+tick_labels = {
+    4: np.arange(0, min_num_iteration, 100),
+    5: np.arange(0, min_num_iteration, 100),
+    3: [0, 15, 30],
+}
+fig.axes[4].xaxis.set_major_locator(MaxNLocator(nbins=8))
+fig.axes[5].xaxis.set_major_locator(MaxNLocator(nbins=8))
+fig.axes[0].xaxis.set_major_locator(MaxNLocator(nbins=8))
+fig.axes[2].xaxis.set_major_locator(MaxNLocator(nbins=8))
+fig.axes[1].xaxis.set_major_locator(MaxNLocator(nbins=3))
+fig.axes[3].xaxis.set_major_locator(MaxNLocator(nbins=3))
+# Apply axis limits and tick labels
+for ax_index, ax in enumerate(fig.axes):
+    if ax_index in x_limits:
+        ax.set_xlim(*x_limits[ax_index])
+    if ax_index in y_limits:
+        ax.set_ylim(*y_limits[ax_index])
+    if ax_index in tick_labels:
+        ax.xaxis.set_major_locator(MaxNLocator(nbins=len(tick_labels[ax_index])))
+        ax.xaxis.set_ticklabels(tick_labels[ax_index])
+legend_list = ["SUEA", "BO-Mord", "BO-Learned", "Rand", "EA"]
+h,l = fig.axes[0].get_legend_handles_labels()
+# Modify the legend
+lg = fig.axes[0].legend(h,
+    legend_list,
+    loc="upper left",  # Change the location of the legend
+    bbox_to_anchor=(0.2, 1.2),  # Adjust the position of the legend
+    ncol=5,  # Number of columns in the legend
+    fontsize=20,  # Font size of the legend text
+
+    frameon=True,  # Add a frame around the legend
+    shadow=False,  # Add a shadow to the legend
+    fancybox=True,  # Use a rounded box for the legend
+    edgecolor="black",  # Set the edge color of the legend box
+)
+
+
+
+fig.tight_layout()
+fig.savefig("mod_fig.png")
+
+# Optionally, save the modified figure as a `.pickle` file for further editing
+with open("mod_fig.pkl", "wb") as f:
+    pickle.dump(fig, f)
+
+# Show the modified figure
